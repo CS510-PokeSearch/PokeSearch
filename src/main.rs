d7e97480205eb602df1d324a9475b484252fb125
rivera26 <rivera26@pdx.edu>
2021-06-08 15:09:52 -0700

    Lowercase fix, invalid search

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,63 @@
 //! https://rocket.rs/v0.4/guide/
 //! https://www.youtube.com/watch?v=2RWXeosWhAQ
 //! https://www.shawntabrizi.com/code/combining-rocket-with-reqwest-to-call-an-api-with-rust/
+//! https://github.com/sunng87/handlebars-rust
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate reqwest;
-#[macro_use] extern crate serde;
-#[macro_use] extern crate serde_derive;
-#[macro_use] extern crate serde_json;
+extern crate serde;
+extern crate reqwest;
+
+#[macro_use] 
+extern crate rocket;
+extern crate serde_derive;
+extern crate serde_json;
 
 use rocket_contrib::serve::StaticFiles;
-use rocket_contrib::templates::Template;
+use rocket_contrib::templates::{Template, handlebars};
+use handlebars::{Helper, Handlebars, Context, RenderContext, Output, RenderError};
 use std::collections::HashMap;
 use rocket::request::LenientForm;
 use serde_json::Value as JsonValue;
 use rocket::response::Redirect;
+use rocket::Request;
+use serde::Serialize;
 
+// handlebars helper to capitalize first letter of strings
+fn capitalize(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> Result<(), RenderError> {
 
-// #[derive(Serialize, Deserialize, Debug)]
-// struct Pokemon {
-//     name: String,
-//     id: u32,
-//     image_url: String,
-// }
+    // helper function to capitalize first letter of str
+    fn cap_first(string: &str) -> String {
+        format!("{}{}", (&string[..1].to_string()).to_uppercase(), &string[1..])
+    }
 
+    let param = h
+        .param(0)
+        .ok_or(RenderError::new("Invalid param."))?;
+
+    let param_cap = cap_first(param.value().as_str().unwrap());
+    
+    let rendered = format!("{}", param_cap);
+    out.write(rendered.as_ref())?;
+    Ok(())
+}
+
+// struct to grab search query
 #[derive(FromForm)]
 struct SearchForm{
     pokemon: String
+}
+
+// struct for invalid search
+#[derive(Serialize, Debug)]
+struct InvalidPokemon {
+    query: String
 }
 
 #[get("/")]
@@ -44,7 +74,6 @@ fn index() -> Template {
 
 #[post("/search", data = "<form>")]
 fn search_form(form: LenientForm<SearchForm>) -> Redirect {
-    println!("POKEMON SEARCH VALUE IS {:?}", form.pokemon);
     Redirect::to(format!("/search/{}", form.pokemon))
 }
 
@@ -54,21 +83,38 @@ fn search(pokemon: String) -> Template {
     let full_url = &base_url[..];
     let client = reqwest::blocking::Client::new();
 
-    let mut response = client.get(full_url)
+    let response = client.get(full_url)
         .send()
         .unwrap(); 
 
-    let data: JsonValue = response.json().unwrap();
-
-    Template::render("search", &data)
+    if response.status().is_success() {
+        let data: JsonValue = response.json().unwrap();
+        Template::render("search", &data)
+    } else {
+        println!{"{:?}", pokemon};
+        let data = InvalidPokemon {
+            query: pokemon,
+        };
+        println!{"{:?}", data.query};
+        Template::render("invalidsearch", &data)
+    }
 }
 
-// implement 404 handling
+// TODO: 404 template
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
+}
 
 fn main() {
     rocket::ignite()
         .mount("/static", StaticFiles::from("static"))
         .mount("/", routes![index, search, search_form])
-        .attach(Template::fairing())
+        .register(catchers![not_found])
+        .attach(Template::custom(|engines| {
+            engines
+                .handlebars
+                .register_helper("capitalize", Box::new(capitalize));
+        }))
         .launch();
 }
